9fb2acde8d17355ef6d4fa70b495858a788f840f
Noah <noah@gangplankhq.com>
2018-06-29 10:05:17 -0700

    added ability to pick characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,56 @@
 window.hero = false;
 window.sith = false;
 
+var yoda = {
+    health: 100,
+    damage: 50,
+};
+
+var obi = {
+    health: 150,
+    damage: 25,
+};
+
+var luke = {
+    health: 175,
+    damage: 20,
+};
+
+var darth = {
+    health: 130,
+    damage: 15,
+};
+
+var general = {
+    health: 120,
+    damage: 15,
+};
+
+var vader = {
+    health: 200,
+    damage: 10,
+}
+
+
 $(document).ready(function () {
-    $(".pick-character").on("click", function (e) {
+    $(".pick-hero").on("click", function (e) {
     if (window.hero === true) {
         return
     }
     window.hero = true;
     $(".hero-span").append($(this));
     });
+
+    $(".pick-sith").on("click", function (e) {
+        if (window.with === true) {
+            return
+        }
+        window.with = true;
+        $(".sith-span").append($(this));
+        });
 });
 
-var pickedCharacters;   
 
-$(document).ready(function (){
-
-$(".picked-character").on("click", function (e){
-for (pickedCharacters = 0; pickedCharacters < 2; pickedCharacters++) {
-       $(".hero-span").append($(this)); 
-    }
-});
-});
 
 
 
