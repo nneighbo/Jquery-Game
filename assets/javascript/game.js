f4fc848d141391628d576b8668743c011e25168f
Noah <noah@gangplankhq.com>
2018-06-30 10:06:21 -0700

    added attack button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,25 +29,49 @@ var general = {
 var vader = {
     health: 200,
     damage: 10,
-}
+};
+
 
 
 $(document).ready(function () {
+
+    $(".attack").css('visibility','hidden'); 
+
     $(".pick-hero").on("click", function (e) {
     if (window.hero === true) {
         return
-    }
+    }   
     window.hero = true;
     $(".hero-span").append($(this));
+    console.log(this);
+});
+
+$(".pick-sith").on("click", function (e) {
+    if (window.with === true) {
+        return
+    }
+    window.with = true;
+        $(".attack").css('visibility','visible');
+        $(".sith-span").append($(this));
     });
 
-    $(".pick-sith").on("click", function (e) {
-        if (window.with === true) {
-            return
-        }
-        window.with = true;
-        $(".sith-span").append($(this));
-        });
+// function attack() {
+//     hero.health - sith.heath;
+//     if (hero.health == 0) {
+//         //game over, reset game
+//         resetGame();
+//     } 
+//     else if (hero.health > sith.health){
+//         //encouage another attach
+        
+//     }
+// }
+
+   var attack = function(e) {
+       if (yoda.health > 1 $$ vader.health > 1) {
+
+       };
+   };
 });
 
 
